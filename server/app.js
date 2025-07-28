f6a4efa78d81160af1c5ad1bc9087b929cf06e72
Your Name <rajukunarapu49@gmail.com>
2025-07-28 14:47:44 +0530

    Used the log statement in cors

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ const allowedOrigins = [`${process.env.VITE_URL}`]
 app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps or curl)
+    console.log("Origin: ", origin);
     if (!origin) return callback(null, true);
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
